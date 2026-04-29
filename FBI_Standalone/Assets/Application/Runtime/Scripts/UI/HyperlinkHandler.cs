0ff367ea16fa3a91a988a2bccfd11ca77e2a9aa6
ArnaudDroxlerEPFL <arnaud.droxler@epfl.ch>
2026-04-29 15:55:50 +0200

    Add footer to legal

---
+++ FBI_Standalone/Assets/Application/Runtime/Scripts/UI/HyperlinkHandler.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+using TMPro;
+
+[RequireComponent(typeof(TMP_Text))]
+public class HyperlinkHandler : MonoBehaviour, IPointerClickHandler
+{
+    [Header("Settings")]
+    public string url = "https://www.epfl.ch/labs/lnco/";
+    public bool openInBrowser = true;
+    public Color hoverColor = new Color(0f, 0.6f, 1f);
+
+    private TMP_Text tmpText;
+    private Canvas canvas;
+    private Camera cam;
+
+    private void Awake()
+    {
+        tmpText = GetComponent<TMP_Text>();
+        canvas = GetComponentInParent<Canvas>();
+
+        if (canvas != null && canvas.renderMode == RenderMode.ScreenSpaceCamera)
+            cam = canvas.worldCamera;
+        else if (canvas != null && canvas.renderMode == RenderMode.WorldSpace)
+            cam = Camera.main;
+        else
+            cam = null;
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+
+        if (openInBrowser)
+        {
+            Application.OpenURL(url);
+            Debug.Log($"[HyperlinkHandler] Opening link: {url}");
+        }
+
+    }
+
+
+}
